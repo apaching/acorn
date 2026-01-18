c2ce352106ff3ed7c46360b2aae2e24f909c393d
Yves Domingo <yvesrafaeladomingo@gmail.com>
2026-01-18 21:56:55 +0800

    test: testing to see if forcing the theme will change ios safari theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,9 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          // defaultTheme="system"
+          // enableSystem
+          forcedTheme="dark"
           disableTransitionOnChange
         >
           <ClientProvider>{children}</ClientProvider>
